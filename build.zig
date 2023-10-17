d860685332616ebe87a8bf7c046ce336400dcdd2
DISTREAT <99132213+DISTREAT@users.noreply.github.com>
2023-10-17 22:20:10 +0200

    build: add option to generate documentation

--- build.zig
+++ build.zig
@@ -11,6 +11,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .{ .custom = ".." },
+        .install_subdir = "docs",
+    });
+
     const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/tests.zig" },
         .target = target,
@@ -28,6 +34,8 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&b.addRunArtifact(main_tests).step);
+    const docs_step = b.step("docs", "Build the documentation");
+    docs_step.dependOn(&install_docs.step);
     const bench_step = b.step("bench", "Run the benchmark test");
     bench_step.dependOn(&b.addRunArtifact(bench_test).step);
 }
